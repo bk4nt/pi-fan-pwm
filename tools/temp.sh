acbdae9ffac9b7aa3e580e60950f354e9a3990c3
bk4nt <53443096+bk4nt@users.noreply.github.com>
2019-12-07 19:22:36 +0100

    Update temp.sh

--- tools/temp.sh
+++ tools/temp.sh
@@ -6,4 +6,6 @@ cpuTemp2=$(($cpuTemp0%1000/100))
 cpuSpeed0=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
 cpuSpeed1=$((cpuSpeed0/1000))
 
+pwmDC=$(cat /run/pi-fan-pwm.dc)
 echo "CPU temp=${cpuTemp1}.${cpuTemp2}'C freq=${cpuSpeed1}kHz"
+echo "Fan duty cycle=${pwmDC}"
